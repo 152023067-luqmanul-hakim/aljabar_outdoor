17c47182cc0fbea8c9e264b3630650ca863d9df9
152023067-luqmanul-hakim <luqmanul.hakim@mhs.itenas.ac.id>
2025-05-26 00:45:07 +0700

    CRUD Produk - admin

--- src/main/java/com/uaspbo/aljabar_outdoor/controller/AdminController.java
+++ src/main/java/com/uaspbo/aljabar_outdoor/controller/AdminController.java
@@ -1,8 +1,17 @@
 package com.uaspbo.aljabar_outdoor.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.uaspbo.aljabar_outdoor.model.Product;
+import com.uaspbo.aljabar_outdoor.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Controller
 @RequestMapping("/admin")
@@ -11,10 +20,21 @@ public class AdminController {
     @GetMapping("/dashboard")
     public String adminDashboard() {
         return "admin/dashboard";
+    }   
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/dataProduct")
+    public String showProductList(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("products", products);
+        return "admin/dataProduct";
     }
 
-    @GetMapping("/products")
-    public String manageProducts() {
-        return "admin/products";
+    @PostMapping("/dataProduct")
+    public String addProduct(Product product) {
+        productRepository.save(product);
+        return "redirect:/admin/dataProduct";
     }
 }
